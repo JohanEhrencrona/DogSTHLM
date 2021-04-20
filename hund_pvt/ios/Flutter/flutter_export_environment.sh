1d037b7ec4049bfedb602db417606e799e6765e4
phba97 <81630494+phba97@users.noreply.github.com>
2021-04-20 14:07:55 +0200

    Första

--- hund_pvt/ios/Flutter/flutter_export_environment.sh
+++ hund_pvt/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/philipbarsk/Documents/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/philipbarsk/Desktop/hund_pvt"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/philipbarsk/Documents/GitHub/DogSTHLM/hund_pvt"
+export "FLUTTER_TARGET=/Users/philipbarsk/Documents/GitHub/DogSTHLM/hund_pvt/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=/Users/philipbarsk/Documents/GitHub/DogSTHLM/hund_pvt/.dart_tool/package_config.json"
