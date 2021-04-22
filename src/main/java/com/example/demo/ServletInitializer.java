ef096d7cbbc5d355fa24ddb3f0d5ac49c46597f7
Jackiliddle <73336687+Jackiliddle@users.noreply.github.com>
2021-04-22 11:55:56 +0200

    Update ServletInitializer.java

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+//package com.example.demo;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+//public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(DemoApplication.class);
+//	}
 
-}
+//}
+//
