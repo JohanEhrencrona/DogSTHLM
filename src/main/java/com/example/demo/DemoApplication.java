72708693e161627fa7dc8113333af11cc5b44515
Jackiliddle <73336687+Jackiliddle@users.noreply.github.com>
2021-04-22 12:19:55 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
+public class DemoApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value ="name", defaultValue = "World") String name) {
+		return String.format("Hello %s", name);
+	}
+
+}
