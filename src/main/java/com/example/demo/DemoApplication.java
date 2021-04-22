4be65f83102f834b5e50ec88b6bb9f63a21a019a
JohanEhrencrona <75279554+JohanEhrencrona@users.noreply.github.com>
2021-04-22 11:31:09 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
 
