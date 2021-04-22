888e3307b00f29c50e27805a460b7eed7583bf79
Jackiliddle <73336687+Jackiliddle@users.noreply.github.com>
2021-04-22 12:46:55 +0200

    Add files via upload

---
+++ src/main/java/com/example/accessingdatamysql/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.example.demo.DemoApplication;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+}
