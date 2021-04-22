e9386ac7316d974e92dc62d2e6f3cbcc933e3c7e
Jackiliddle <73336687+Jackiliddle@users.noreply.github.com>
2021-04-22 12:01:30 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,25 +1,24 @@
-//package com.example.demo;
+package com.example.demo;
 
-//import org.springframework.boot.SpringApplication;
-//<!--- import org.springframework.boot.autoconfigure.SpringBootApplication;
-//////import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//////import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-//@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 
-//public class DemoApplication extends SpringBootServletInitializer{
+public class DemoApplication extends SpringBootServletInitializer{
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
-//	}
-//	@GetMapping("/hello")
-//	public String hello(@RequestParam(value ="name", defaultValue = "World") String name) {
-//		return String.format("Hello %s", name);
-//	}
-//
-//} ---!>
-////
+public static void main(String[] args) {
+SpringApplication.run(DemoApplication.class, args);
+}
+@GetMapping("/hello")
+public String hello(@RequestParam(value ="name", defaultValue = "World") String name) {
+return String.format("Hello %s", name);
+}
+
+}
