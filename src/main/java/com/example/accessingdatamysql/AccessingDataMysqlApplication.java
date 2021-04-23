f03ebc5f72f303729ff9d6eccbfee36ac5d03e9c
JohanEhrencrona <johanehrencrona@gmail.com>
2021-04-23 13:21:16 +0200

    Commited adding trashcans to mysql

--- src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccessingDataMysqlApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/app");
 		SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
 }
