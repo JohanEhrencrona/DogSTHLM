1e7d0a8db97a29ea74a38da10e0a8885f44eab90
Joal5094 <Jallgulin@gmail.com>
2021-04-29 13:11:12 +0200

    Custom Icons for a marker

--- hund_pvt/lib/Pages/home.dart
+++ hund_pvt/lib/Pages/home.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 List<Marker> _testMarkers = <Marker>[];
+List<BitmapDescriptor> _customIcons = <BitmapDescriptor>[];
+String _mapStyle;
 
 class Home extends StatefulWidget {
   @override
@@ -16,6 +18,7 @@ Completer<GoogleMapController> _controller = Completer();
 
 void _onMapCreated(GoogleMapController controller) {
   _controller.complete(controller);
+  controller.setMapStyle(_mapStyle);
 }
 
 class _HomeState extends State<Home> {
@@ -25,15 +28,32 @@ class _HomeState extends State<Home> {
   void initState() {
     getIcons();
     super.initState();
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      _mapStyle = string;
+    });
   }
 
   getIcons() async {
-    final bitmapIcon = await BitmapDescriptor.fromAssetImage(
+    final cafeIcon = await BitmapDescriptor.fromAssetImage(
         ImageConfiguration(size: Size(0, 0)), 'assets/images/Cafe.png');
+    _customIcons.add(cafeIcon);
+    final parkIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(0, 0)), 'assets/images/Dog_Park.png');
+    _customIcons.add(parkIcon);
+    final restaurantIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(0, 0)), 'assets/images/Restaurants.png');
+    _customIcons.add(restaurantIcon);
+    final trashIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(0, 0)), 'assets/images/Trash_Cans.png');
+    _customIcons.add(trashIcon);
+    final vetIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(0, 0)), 'assets/images/Vet.png');
+    _customIcons.add(vetIcon);
+
     _testMarkers.add(Marker(
         markerId: MarkerId('ID'),
         position: LatLng(59.3360198, 18.0297926),
-        icon: bitmapIcon,
+        icon: _customIcons.elementAt(0),
         infoWindow: InfoWindow(title: 'Marker title')));
     setState(() {});
   }
@@ -77,15 +97,15 @@ class _HomeState extends State<Home> {
             items: [
               BottomNavigationBarItem(
                 icon: Icon(Icons.favorite),
-                title: Text("Favorite"),
+                label: ("Favorite"),
               ),
               BottomNavigationBarItem(
                 icon: Icon(Icons.filter_alt_rounded),
-                title: Text("Filter"),
+                label: ("Filter"),
               ),
               BottomNavigationBarItem(
                 icon: Icon(Icons.search),
-                title: Text("Search"),
+                label: ("Search"),
               ),
             ],
             onTap: (index) {
