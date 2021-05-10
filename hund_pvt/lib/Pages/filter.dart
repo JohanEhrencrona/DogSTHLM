78f1d91a16e3f7778058c35c9d5d800742e14c34
jossankuylser <jossankuylser@gmail.com>
2021-05-10 12:02:08 +0200

    change to gradient colors

--- hund_pvt/lib/Pages/filter.dart
+++ hund_pvt/lib/Pages/filter.dart
@@ -11,37 +11,52 @@ List<CheckBoxListTileModel> checkBoxListTileModel =
 class FilterScreenState extends State<FilterScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Container(
+      decoration: BoxDecoration(gradient: 
+              LinearGradient(begin:
+              Alignment.topCenter, end: Alignment.bottomCenter, colors: 
+              <Color>[
+                Color(0xffDD5151), Color(0xff583177)
+              ])),
+            
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
         appBar: AppBar(
+          backgroundColor: Colors.transparent,
           title: Text('Filter'),
-          backgroundColor: Colors.pink,
         ),
         body: ListView.builder(
           itemCount: checkBoxListTileModel.length,
           itemBuilder: (BuildContext context, int index) {
             return new Card(
+              color: Colors.transparent,
                 child: new Container(
               padding: new EdgeInsets.all(8),
               child: Column(
                 children: <Widget>[
-                  new CheckboxListTile(
-                    activeColor: Colors.red,
-                    dense: true,
-                    title: Text(checkBoxListTileModel[index].filtername),
+                  Theme( //Gör så att man får vita borders på checkboxarna
+                    data: ThemeData(unselectedWidgetColor: Colors.white), //vita färgen väljs
+                    child: new CheckboxListTile(
+                    activeColor: Colors.transparent,
+                    checkColor: Colors.white,
+                    dense: false,
+                    title: Text(checkBoxListTileModel[index].filtername, style: TextStyle(color: Colors.white),),
                     value: checkBoxListTileModel[index].isChecked,
-                    secondary: Container(
-                      height: 50,
-                      width: 50,
+                    controlAffinity: ListTileControlAffinity.leading,
+                    secondary: ImageIcon(
+                      checkBoxListTileModel[index].imageTest,
+                      color: Colors.white,
                     ),
                     onChanged: (bool val) {
                       itemChange(val, index);
                     },
                   )
+                  )
                 ],
               ),
             ));
           },
-        ));
+        )));
   }
 
   void itemChange(bool val, int index) {
@@ -54,18 +69,19 @@ class FilterScreenState extends State<FilterScreen> {
 class CheckBoxListTileModel {
   String filtername;
   bool isChecked;
+  AssetImage imageTest;
 
-  CheckBoxListTileModel({this.filtername, this.isChecked});
+  CheckBoxListTileModel({this.filtername, this.isChecked, this.imageTest});
 
   static List<CheckBoxListTileModel> getFilters() {
     return <CheckBoxListTileModel>[
-      CheckBoxListTileModel(filtername: 'Skräpkorgar', isChecked: true),
-      CheckBoxListTileModel(filtername: 'Hundparker', isChecked: true),
-      CheckBoxListTileModel(filtername: 'Placeholder', isChecked: false),
-      CheckBoxListTileModel(filtername: 'Placeholder', isChecked: false),
-      CheckBoxListTileModel(filtername: 'Placeholder', isChecked: false),
-      CheckBoxListTileModel(filtername: 'Placeholder', isChecked: false),
-      CheckBoxListTileModel(filtername: 'Placeholder', isChecked: false),
+      CheckBoxListTileModel(filtername: 'Skräpkorgar', isChecked: true, imageTest: AssetImage("assets/images/trashcan_symbol.png") ),
+      CheckBoxListTileModel(filtername: 'Hundparker', isChecked: true, imageTest: AssetImage("assets/images/dog_symbol.png")),
+      CheckBoxListTileModel(filtername: 'Veterinärer', isChecked: false, imageTest: AssetImage("assets/images/veterinary_symbol.png")),
+      CheckBoxListTileModel(filtername: 'Caféer', isChecked: false, imageTest: AssetImage("assets/images/cafe_symbol.png")),
+      CheckBoxListTileModel(filtername: 'Restauranger', isChecked: false, imageTest: AssetImage("assets/images/restaurant_symbol.png")),
+      CheckBoxListTileModel(filtername: 'Djurbutiker', isChecked: false, imageTest: AssetImage("assets/images/bone_symbol.png")),
+      CheckBoxListTileModel(filtername: 'Placeholder', isChecked: false, imageTest: AssetImage("assets/images/pawer.png")),
     ];
   }
 }
